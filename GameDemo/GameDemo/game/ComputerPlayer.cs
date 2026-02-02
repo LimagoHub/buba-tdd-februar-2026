aa16750740d4053ab48cc70e9ddcfb893c912c01
Jo Wagner <jowagner@limago.de>
2026-02-02 14:22:32 +0100

    initial commit

---
+++ GameDemo/GameDemo/game/ComputerPlayer.cs
@@ -0,0 +1,15 @@
+﻿using GameDemo.io;
+
+namespace GameDemo.game;
+
+public class ComputerPlayer(IWriter writer)
+{
+    private static readonly int[] Moves = [3, 1, 1, 2];
+
+    public int DoTurn(int stones)
+    {
+        var move = Moves[stones % 4];
+        writer.Write($"Computer nimmt {move} Steine");
+        return move;
+    }
+}
